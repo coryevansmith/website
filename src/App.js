9e0b578fff520b3317b4a9704d165a6ed64f69a5
coryevansmith <coryevansmith@gmail.com>
2024-04-30 19:14:25 -0500

    First official build with nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Button, Container } from "@mui/material";
+import Home from "./home";
+import Resume from "./resume";
+import logo from "./resources/logo.png";
 
 function App() {
   return (
-    <div className="App">
-      <header>UNDER CONSTRUCTION</header>
-    </div>
+    <Router>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <img
+              src={logo}
+              alt="Logo"
+              style={{ marginRight: "10px", height: "50px" }}
+            />
+          </Typography>
+          <Button color="inherit" href="/">
+            Home
+          </Button>
+          <Button color="inherit" href="/resume">
+            Resume
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Container sx={{ marginTop: "20px" }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+      </Container>
+    </Router>
   );
 }
 
---
+++ src/home.js
@@ -0,0 +1,8 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+function Home() {
+  return <Typography variant="h1">UNDER CONSTRUCTION</Typography>;
+}
+
+export default Home;
