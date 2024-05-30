f150d0e392e95a68777175355bca1d454128818b
Corbert <coryevansmith@gmail.com>
2024-05-29 22:10:12 -0500

    font change

--- src/theme.js
+++ src/theme.js
@@ -2,6 +2,20 @@ import { red } from "@mui/material/colors";
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
+  typography: {
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+  },
   palette: {
     primary: {
       main: "#222222",
