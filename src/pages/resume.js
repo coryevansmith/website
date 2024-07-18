ca3a952044646300a7c6c8f9ed18119e35d801dd
coryevansmith <coryevansmith@gmail.com>
2024-07-18 09:34:37 -0500

    adding loading for resume page

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,19 +1,38 @@
-import React from "react";
+import { CircularProgress } from "@mui/material";
+import React, { useState } from "react";
 
 function Resume() {
+  const [loading, setLoading] = useState(true);
+
   const iframeStyle = {
     width: "100%",
     border: "none",
     height: "100vh",
+  };
 
+  const handleLoad = () => {
+    setLoading(false);
   };
 
   return (
-    <iframe
-      src={process.env.PUBLIC_URL + "/resume.pdf"}
-      style={iframeStyle}
-      title="resume"
-    />
+    <>
+      {loading && (
+        <CircularProgress
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+        />
+      )}
+      <iframe
+        src={process.env.PUBLIC_URL + "/resume.pdf"}
+        style={iframeStyle}
+        onLoad={handleLoad}
+        title="resume"
+      />
+    </>
   );
 }
 
