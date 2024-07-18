f0ea4c9c5867c7b121fa7cf64d5b9096811789cb
coryevansmith <coryevansmith@gmail.com>
2024-07-18 07:47:50 -0500

    adding loading to gallery and contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,17 @@
-import { Box } from "@mui/material";
-import React from "react";
+import { Box, CircularProgress } from "@mui/material";
+import React, { useState } from "react";
 
 function Contact() {
+  const [loading, setLoading] = useState(true);
+
   const iframeStyle = {
     width: "100%",
     border: "none",
     height: "100vh",
+  };
+
+  const handleLoad = () => {
+    setLoading(false);
   };
 
   return (
@@ -19,13 +25,22 @@ function Contact() {
           "linear-gradient(13deg, rgba(34,34,34,0.5830707282913166) 1%, rgba(246,246,246,1) 100%)",
       }}
     >
+      {loading && (
+        <CircularProgress
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+        />
+      )}
       <iframe
         src="https://docs.google.com/forms/d/e/1FAIpQLScc2ln5n13BB2I-wijN2-lDLurhHTKyElIuBeExxWjV2r3Y4Q/viewform?embedded=true"
         style={iframeStyle}
+        onLoad={handleLoad}
         title="contact"
-      >
-        Loading…
-      </iframe>
+      />
     </Box>
   );
 }
--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,8 +1,24 @@
-import { Box, ImageList, ImageListItem, ImageListItemBar } from "@mui/material";
-import React from "react";
+import { Box, ImageList, ImageListItem, CircularProgress } from "@mui/material";
+import React, { useState, useEffect } from "react";
 import imageList from "../handlers/galleryHandler";
 
 function Gallery() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const imagePromises = imageList.map((item) => {
+      return new Promise((resolve) => {
+        const img = new Image();
+        img.src = `${item.img}?w=248&fit=crop&auto=format&dpr=2`;
+        img.onload = resolve;
+      });
+    });
+
+    Promise.all(imagePromises).then(() => {
+      setLoading(false);
+    });
+  }, []);
+
   return (
     <Box
       style={{
@@ -13,25 +29,39 @@ function Gallery() {
         background: "rgb(34,34,34)",
         background:
           "linear-gradient(13deg, rgba(34,34,34,0.5830707282913166) 1%, rgba(246,246,246,1) 100%)",
+        position: "relative",
+        width: "100%",
       }}
     >
-      <ImageList sx={{ width: "80%" }}>
-        {imageList.map((item) => (
-          <ImageListItem key={item.img}>
-            <img
-              srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-              src={`${item.img}?w=248&fit=crop&auto=format`}
-              alt={item.title}
-              loading="lazy"
-            />
-            {/* <ImageListItemBar
-              title={item.title}
-              subtitle={<span>{item.desc}</span>}
-              position="below"
-            /> */}
-          </ImageListItem>
-        ))}
-      </ImageList>
+      {loading && (
+        <CircularProgress
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+        />
+      )}
+      {!loading && (
+        <ImageList sx={{ width: "80%" }}>
+          {imageList.map((item) => (
+            <ImageListItem key={item.img}>
+              <img
+                srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                src={`${item.img}?w=248&fit=crop&auto=format`}
+                alt={item.title}
+                loading="lazy"
+              />
+              {/* <ImageListItemBar
+                title={item.title}
+                subtitle={<span>{item.desc}</span>}
+                position="below"
+              /> */}
+            </ImageListItem>
+          ))}
+        </ImageList>
+      )}
     </Box>
   );
 }
