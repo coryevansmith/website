dad5111820a2ce5360513a1b862ef94f6b315fad
Cory Smith <65304167+coryevansmith@users.noreply.github.com>
2024-12-16 20:47:06 +0000

    Preparing home page for larger changes

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,18 +1,21 @@
 import { Box, Typography } from "@mui/material";
 import React from "react";
-import backgroundPhoto from "../resources/frontPage.png";
+// import backgroundPhoto from "../resources/frontPage.png";
 
 function Home() {
   return (
     <Box
       minHeight="100vh"
       sx={{
-        backgroundImage: `url(${backgroundPhoto})`,
+        // backgroundImage: `url(${backgroundPhoto})`,
+        background: "rgb(34,34,34)",
+        background:
+          "linear-gradient(13deg, rgba(34,34,34,0.5830707282913166) 1%, rgba(246,246,246,1) 100%)",
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
     >
-      <Typography variant="h1">CS Development</Typography>
+      <Typography variant="h1">HOME PAGE UNDER CONSTRUCTION</Typography>
     </Box>
   );
 }
