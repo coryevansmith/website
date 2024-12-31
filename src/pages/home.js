2c036f530f5789a50a8a1f30a3a1b6f949811aae
Cory Smith <65304167+coryevansmith@users.noreply.github.com>
2024-12-31 14:14:04 +0000

    Starting changes to homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,21 +1,65 @@
-import { Box, Typography } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Grid,
+  Card,
+  CardContent,
+  CardMedia,
+} from "@mui/material";
 import React from "react";
-// import backgroundPhoto from "../resources/frontPage.png";
+import homeImage from "../resources/profileImage.jpg";
 
 function Home() {
   return (
     <Box
-      minHeight="100vh"
-      sx={{
-        // backgroundImage: `url(${backgroundPhoto})`,
-        background: "rgb(34,34,34)",
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
         background:
           "linear-gradient(13deg, rgba(34,34,34,0.5830707282913166) 1%, rgba(246,246,246,1) 100%)",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
       }}
     >
-      <Typography variant="h1">HOME PAGE UNDER CONSTRUCTION</Typography>
+      <Grid container>
+        <Grid style={{ marginLeft: "2rem" }}>
+          <Card
+            style={{
+              width: "30vw",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "stretch",
+              marginTop: "2rem",
+              marginBottom: "2rem",
+            }}
+          >
+            <CardMedia
+              component="img"
+              image={homeImage}
+              alt="Home Image"
+              style={{ height: "100%", width: "100%", objectFit: "cover" }}
+            />
+          </Card>
+        </Grid>
+        <Grid style={{ marginLeft: "2rem" }}>
+          <Card
+            style={{
+              width: "60vw",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              marginTop: "2rem",
+              marginBottom: "2rem",
+            }}
+          >
+            <CardContent>
+              <Typography variant="h3" gutterBottom>
+                Cory Smith
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </Box>
   );
 }
